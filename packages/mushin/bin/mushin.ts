0e6304ac07270f47cc55923142e09e1a989bad36
setobox <setoboxes@gmail.com>
2023-01-29 18:19:33 +0800

    chore: change to vite

--- packages/mushin/bin/mushin.ts
+++ packages/mushin/bin/mushin.ts
@@ -4,8 +4,6 @@ console.log('mushin cli')
 
 async function start() {
   console.log('start')
-  process.env.HOST = 'localhost'
-  process.env.PORT = '3517'
   await execa('node', ['node_modules/@mushinjs/console/.output/server/index.mjs']).stdout!.pipe(process.stdout)
 }
 
