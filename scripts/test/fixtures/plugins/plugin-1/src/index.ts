f8f33401bbe0826e27dfae2e16ad80341c12e3ce
backgron <1319536124@qq.com>
2023-03-01 15:34:29 +0800

    wip: dts develop

--- scripts/test/fixtures/plugins/plugin-1/src/index.ts
+++ scripts/test/fixtures/plugins/plugin-1/src/index.ts
@@ -1,5 +1,16 @@
 import type { Foo } from 'package-2'
-import { hello, world } from 'package-2'
+import { Person, hello, world } from 'package-2'
 
 hello() as unknown as Foo<string>
 world()
+
+declare module 'package-2' {
+  namespace Person{
+    export function sayBye(): void
+  }
+}
+
+const p = new Person()
+p.name = '123'
+Person.sayHi()
+Person.sayBye()
