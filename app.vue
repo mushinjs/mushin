f8bc75f0ff5d1953f781a22447516a873e5df50e
Setobox <setoboxes@gmail.com>
2023-11-22 19:59:41 +0800

    chore: basic modules setup

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+.dark {
+  color-scheme: dark;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { x, y } = useMouse()
+
+const color = useColorMode()
+function toggleDarkMode() {
+  color.value = color.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <div text-red>
+    Hello World {{ x }}, {{ y }}
+    <button @click="toggleDarkMode">
+      {{ color.value }}
+    </button>
+  </div>
+</template>
